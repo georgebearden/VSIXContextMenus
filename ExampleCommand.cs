e7fd092ef6ee5edd35cba8d71d5b581aff1c9fa7
George Bearden <george@goerden.com>
2016-01-08 22:35:31 -0500

    Adding an example tool menu item

---
+++ ExampleCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using Microsoft.VisualStudio.Shell;
+
+namespace VSIXContextMenus
+{
+    internal sealed class ExampleCommand
+    {
+        public const int CommandId = 0x0100;
+        public static readonly Guid CommandSet = new Guid("b827c1d5-b9e8-4370-9451-a60a3e14d7ce");
+        readonly Package package;
+
+        ExampleCommand(Package package)
+        {
+            if (package == null)
+                throw new ArgumentNullException(nameof(package));
+
+            this.package = package;
+
+            var commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandId = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandId);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        public static void Initialize(Package package)
+        {
+            Instance = new ExampleCommand(package);
+        }
+
+        void MenuItemCallback(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Hello World");
+        }
+
+        public static ExampleCommand Instance { get; private set; }
+        IServiceProvider ServiceProvider => this.package;
+    }
+}
---
+++ ExampleCommandPackage.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace VSIXContextMenus
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(ExampleCommandPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class ExampleCommandPackage : Package
+    {
+        public const string PackageGuidString = "8d4ffc10-75b7-4af3-8c8d-7741a71c3699";
+
+        protected override void Initialize()
+        {
+            ExampleCommand.Initialize(this);
+            base.Initialize();
+        }
+    }
+}
